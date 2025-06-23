42884d4ece44215d4028c02fe5beaf57a9bb7f21
Digvijay-x1 <hackerearthx1@gmail.com>
2025-06-24 02:30:19 +0530

    added the feature to upload img in the backend

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
 
 const ChatContainer = () => {
+  const { selectedUser, getMessages, messages, isMessagesLoading } = useChatStore();
+
+  useEffect(()=>{
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+
+  if(isMessagesLoading) return (
+    <div className="flex-1 flex flex-col overflow-auto">      <ChatHeader/>
+    <MessageSkeleton/>
+    <MessageInput/>
+  
+  </div>
+  )
   return (
-    <div>ChatContainer</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader/>
+      <p>messages...</p>
+      <MessageInput/>
+    </div>
   )
 }
 

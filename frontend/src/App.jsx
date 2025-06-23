5d3d5fd920ed6f8ffc1e464028a4cdcd25ede220
Digvijay-x1 <hackerearthx1@gmail.com>
2025-06-23 23:30:34 +0530

    made a theme page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,14 @@ import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore'
+import { useThemeStore } from './store/useThemeStore'
 import { Loader } from 'lucide-react'
 import { Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
-
+  const {theme} = useThemeStore();
   useEffect(()=>{
     checkAuth();
   },[checkAuth])
@@ -27,7 +28,7 @@ const App = () => {
     )
   }
   return (
-   <div data-theme="dark">
+   <div data-theme={theme}>
     <Navbar />
     <Routes>
       <Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />

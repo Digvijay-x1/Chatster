1d06a8aef2a95e81c324a61ace01e9972f563f2b
Digvijay-x1 <hackerearthx1@gmail.com>
2025-06-24 18:59:02 +0530

    ready for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,10 @@ dotenv.config();
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app , server } from "./lib/socket.js";
+import path from "path";
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -19,6 +21,14 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"))
+    })
+}
 
 server.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3333/api',
+    baseURL: import.meta.env.MODE === 'production' ? '/api' : 'http://localhost:3333/api',
     withCredentials: true,
     timeout: 60000, // 60 seconds timeout for larger uploads
     maxContentLength: 50 * 1024 * 1024, // 50MB max content length
